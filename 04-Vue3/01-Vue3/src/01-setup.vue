a4e0f1cb74faa2e2db063ce7d1f8dfe5532774e7
zazhi <2065707071@qq.com>
2025-12-12 00:57:14 +0800

    feat: initialize Vue 3 project with Vite, add basic structure and configuration files

---
+++ 04-Vue3/01-Vue3/src/01-setup.vue
@@ -0,0 +1,32 @@
+<!--
+<script>
+export default {
+  setup() {
+    const message = "Hello Vue 3!";
+    const logMessage = () => {
+      console.log(message);
+    };
+    return {
+      message,
+      logMessage
+    };
+  }
+}
+</script>
+-->
+
+<script setup>
+const message = "Hello Vue 3!";
+const logMessage = () => {
+  console.log(message);
+}
+</script>
+
+
+<template>
+  <div>{{ message }}</div>
+  <button @click="logMessage">Log Message</button>
+</template>
+
+<style scoped>
+</style>
---
+++ 04-Vue3/01-Vue3/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {reactive} from "vue";
+
+// reactive: 使得对象变成响应式的
+const state = reactive({
+  count: 0
+})
+const addOne = () => {
+  state.count += 1;
+}
+
+// （🌟推荐）ref: 使得基本类型或者复杂类型变成响应式的 （原理是包装成对象再用reactive）
+import {ref} from "vue";
+const countRef = ref(0);
+const addOneRef = () => {
+  countRef.value += 1;
+}
+
+</script>
+
+<template>
+  <div>{{ state.count }}</div>
+  <button @click="addOne">+ 1</button>
+  <hr>
+  <div>{{ countRef }}</div>
+  <button @click="addOneRef">+ 1 Ref</button>
+</template>
+
+<style scoped>
+</style>
